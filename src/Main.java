5ea9375dcd276d8897b3921dbbbea8d9221a746d
ssabchobo <lch9502@naver.com>
2020-01-23 16:08:30 +0900

    백준10872

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,14 @@ import com.sun.scenario.effect.impl.sw.java.JSWColorAdjustPeer;
 import java.util.*;
 
 public class Main{
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int number = in.nextInt();
 
-        for(int i=1;i<=number;i++) {
-            for(int j=1; j<=number-i;j++){
-                System.out.print(" ");
-            }
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
+        int result = 1;
+        for(int i=1;i<=number;i++){
+            result *= i;
         }
+        System.out.println(result);
     }
 }
