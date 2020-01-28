82b939700d883ac592861c2c8a3998583be7f2e3
ssabchobo <lch9502@naver.com>
2020-01-28 14:16:55 +0900

    백준11052

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,20 @@ import java.util.*;
 public class Main{
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int number = in.nextInt();
+        int num1 = in.nextInt();
 
-        int result = 1;
-        for(int i=1;i<=number;i++){
-            result *= i;
+        int[] arr = new int[num1+1];
+        int[] D = new int[num1+1];
+
+        for(int i = 1; i<=num1;i++){
+            arr[i] = in.nextInt();
         }
-        System.out.println(result);
+
+        for(int i=1;i<=num1;i++){
+            for(int j=1;j<=i;j++){
+                D[i] = Math.max(D[i],arr[j]+D[i-j]);
+            }
+        }
+        System.out.println(D[num1]);
     }
 }
