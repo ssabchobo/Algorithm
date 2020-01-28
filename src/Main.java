2229e16ae24cfaed66d6caf5e11085b0161f5699
ssabchobo <lch9502@naver.com>
2020-01-28 15:40:28 +0900

    백준16194

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,22 @@ import java.util.*;
 public class Main{
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int num1 = in.nextInt();
+        int n = in.nextInt();
 
-        int[] arr = new int[num1+1];
-        int[] D = new int[num1+1];
+        int[] price = new int[n+1];
+        int[] D = new int[n+1];
 
-        for(int i = 1; i<=num1;i++){
-            arr[i] = in.nextInt();
+        for(int i = 1; i<=n;i++){
+            price[i] = in.nextInt();
         }
 
-        for(int i=1;i<=num1;i++){
+        for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
-                D[i] = Math.max(D[i],arr[j]+D[i-j]);
+                if(D[i]==0||D[i]>D[i-j]+price[j]) {
+                    D[i] = price[j] + D[i - j];
+                }
             }
         }
-        System.out.println(D[num1]);
+        System.out.println(D[n]);
     }
 }
