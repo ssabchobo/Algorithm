e91b0844ccbd57d3940fd20b4d099a80cd48af8d
ssabchobo <lch9502@naver.com>
2020-01-29 18:28:52 +0900

    백준 2798

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,30 @@ import com.sun.scenario.effect.impl.sw.java.JSWColorAdjustPeer;
 import java.util.*;
 
 public class Main{
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
+        int n = in.nextInt(); //n입력
+        int m = in.nextInt(); //m입력
 
-        int[] arr = new int[n+1];
+        int[] arr = new int[n]; //n만큼 정수카드 입력
+        //배열 초기화
+        for(int i=0;i<n;i++){
+            arr[i] = in.nextInt();
+        }
 
-        for(int i=0;i<=n;i++){
-            if(i<3){
-                arr[i]=i;
-            }else{
-                arr[i] = arr[i - 1] + arr[i - 2];
-                arr[i] %= 10007;
+        int result = 0; //3장의 카드값 결과
+        int max=0; //max값
+
+        for(int i=0;i<n-2;i++) {
+            for (int j = i + 1; j < n-1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    result = arr[i] + arr[j] + arr[k];
+                    if (result <= m && result > max) {
+                        max = result;
+                    }
+                }
             }
         }
-        System.out.println(arr[n]);
+        System.out.println(max);
     }
 }
