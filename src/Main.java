2aeae144c77e8a3b004639c52fa86ff9d061cae6
ssabchobo <lch9502@naver.com>
2020-02-03 16:43:32 +0900

    백준 15649

--- src/Main.java
+++ src/Main.java
@@ -3,30 +3,34 @@ import com.sun.scenario.effect.impl.sw.java.JSWColorAdjustPeer;
 import java.util.*;
 
 public class Main{
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
+    static int n,m;
+    static int list[],check[];
 
-        int[] arr = new int[n+1];
-        if(n==0){
-            arr[0] = 0;
-        }
-        else if(n==1) {
-            arr[1] = 1;
-        }
-        else if(n==2){
-            arr[2] = 3;
-        }
-        else if(n>=3){
-            arr[1]=1;
-            arr[2]=3;
-            for (int i = 3; i <= n; i++) {
-                for (int j = i; j <= n; j++) {
-                    arr[j] = 2 * arr[j - 2] + arr[j - 1];
-                    arr[j] %= 10007;
-                }
+    static void dfs(int cnt){
+        if(cnt==m){
+            for(int i=0;i<m;i++) {
+                System.out.print(list[i] + " ");
             }
+            System.out.println();
         }
-        System.out.println(arr[n]);
+        for(int i=1;i<=n;i++){
+            if(check[i]==1){
+                continue;
+            }
+            check[i]=1;
+            list[cnt]=i;
+            dfs(cnt+1);
+            check[i]=0;
+        }
+    }
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+        m = in.nextInt();
+
+        list = new int[9];
+        check = new int[9];
+        dfs(0);
     }
 }
+
