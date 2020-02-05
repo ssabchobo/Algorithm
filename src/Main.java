4e64151e87ec50798e22129f7dfc31bf1c05e5c8
ssabchobo <lch9502@naver.com>
2020-02-05 11:10:36 +0900

    백준 10870

--- src/Main.java
+++ src/Main.java
@@ -3,34 +3,19 @@ import com.sun.scenario.effect.impl.sw.java.JSWColorAdjustPeer;
 import java.util.*;
 
 public class Main{
-    static int n,m;
-    static int list[],check[];
-
-    static void dfs(int cnt){
-        if(cnt==m){
-            for(int i=0;i<m;i++) {
-                System.out.print(list[i] + " ");
-            }
-            System.out.println();
-        }
-        for(int i=1;i<=n;i++){
-            if(check[i]==1){
-                continue;
-            }
-            check[i]=0;
-            list[cnt]=i;
-            check[i]=0;
-            dfs(cnt+1);
-        }
-    }
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        n = in.nextInt();
-        m = in.nextInt();
+        int n = in.nextInt();
 
-        list = new int[9];
-        check = new int[9];
-        dfs(0);
+        int[] arr = new int[1001];
+
+        arr[0]= 0;
+        arr[1]= 1;
+
+        for(int i=2;i<=n;i++){
+            arr[i] = arr[i-1]+arr[i-2];
+        }
+        System.out.println(arr[n]);
+
     }
 }
-
