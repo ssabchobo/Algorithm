34253c3aa55c0048d281cebe5abd4082006f604e
ssabchobo <lch9502@naver.com>
2020-02-09 17:47:37 +0900

    백준 2839

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,23 @@ import com.sun.scenario.effect.impl.sw.java.JSWColorAdjustPeer;
 import java.util.*;
 
 public class Main {
-    long sum(int[] a){
-        long ans = 0;
-        for(int i = 0; i<a.length; i++){
-            ans += a[i];
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        long num = in.nextInt();
+        long count=0;
+
+        while(true){
+            if(num%5==0){
+                System.out.println(num/5+count);
+                break;
+            }
+            else if(num<=0){
+                System.out.println("-1");
+                break;
+            }
+            num = num-3;
+            count++;
         }
-        return ans;
+
     }
 }
