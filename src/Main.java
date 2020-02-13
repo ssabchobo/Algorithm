758fcae456197637d605f10cf10c1236327d6176
ssabchobo <lch9502@naver.com>
2020-02-13 21:49:25 +0900

    백준 MN미완성

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,29 @@
+import java.sql.SQLOutput;
 import java.util.*;
 
 public class Main{
     static int n,m;
     static int[] result = new int[9];
+    static boolean[] visit = new boolean[9];
     static int[] arr;
+    static HashSet set = new HashSet();
+    static Iterator it = set.iterator();
+    static String str;
 
     public static void loop(int level){
         if(level == m){
             for (int i = 0; i < m; i++) {
-                System.out.print(result[i]+" ");
+                str = result[i]+" ";
             }
             System.out.println();
             return;
         }
         for (int i = 0; i < n; i++) {
             result[level] = arr[i];
+            if(visit[i])continue;
+            visit[i] = true;
             loop(level+1);
+            visit[i] = false;
         }
     }
     public static void main(String[] args){
@@ -28,5 +36,10 @@ public class Main{
         }
         Arrays.sort(arr);
         loop(0);
+
+        set.add(str);
+        while(it.hasNext()) {
+            System.out.print(it.next());
+        }
     }
 }
