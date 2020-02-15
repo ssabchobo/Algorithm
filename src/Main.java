a62662e0a14a5b4be49b79862ebdef5232db2f62
ssabchobo <lch9502@naver.com>
2020-02-15 19:20:42 +0900

    백준 2231

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,27 @@
-import java.sql.SQLOutput;
 import java.util.*;
 
 public class Main{
-    static int n,m;
-    static int[] result = new int[9];
-    static boolean[] visit = new boolean[9];
-    static int[] arr;
-    static HashSet<String> set = new HashSet<String>();
-    static Iterator it = set.iterator();
-    static String[] ans;
-    static String str="";
 
-    public static void loop(int level){
-        if(level == m){
-            for (int i = 0; i < m; i++) {
-                str += result[i]+" ";
-            }
-            set.add(str);
-            str = "";
-            return;
+    public static int Sum(int a){
+        int sum = 0;
+        while(a>0){
+            sum += a%10;
+            a /=10;
         }
-        for (int i = 0; i < n; i++) {
-            result[level] = arr[i];
-            if(visit[i])continue;
-            visit[i] = true;
-            loop(level+1);
-            visit[i] = false;
-        }
+        return sum;
     }
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        n = in.nextInt();
-        m = in.nextInt();
-        arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = in.nextInt();
-        }
-        Arrays.sort(arr);
-        loop(0);
+        int input = in.nextInt();
+        int result=0;
 
-
-        ans = set.toArray(new String[0]);
-        Arrays.sort(ans);
-        for(String s : ans){
-            System.out.println(s);
+        for (int i = 1; i < input; i++) {
+            int sum = Sum(i);
+            if(i+sum == input){
+                result = i;
+                break;
+            }
         }
+        System.out.println(result);
     }
 }
