591b0d524702251dc1c5401b4ffbd13da03d5470
ssabchobo <lch9502@naver.com>
2020-04-19 17:24:33 +0900

    백준 2441

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,11 @@ public class Main{
         int input;
         input = in.nextInt();
 
-        for (int i = 1; i <= input; i++) {
-            for (int j = 1; j <= i; j++) {
+        for (int i = input; i >= 1; i--) {
+            for (int j = 0; j < input-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j >= 1; j--) {
                 System.out.print("*");
             }
             System.out.println();
